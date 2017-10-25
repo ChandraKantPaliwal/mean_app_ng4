c687bea2f83190d8f69b7af16e1b78fb2ede0913
HaiderMalik <haidermalik504@gmail.com>
2017-10-25 22:44:58 +0500

    connected to mognoose

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.get('/',(req,res) => {
     return res.send('Welcome to MEAN App with Angular4+');
 });
+const MONGO_DB_URI = 'mongodb://localhost/y_mean';
+mongoose.connect(MONGO_DB_URI,{
+   useMongoClient: true
+});
 
+mongoose.connection.on('connected',() => {
+    console.log('app is connected to mongodb ', MONGO_DB_URI);
+});
+
+mongoose.connection.on('error', err => {
+    console.log('error while connecting to mongoose ', err);
+});
 
 app.listen('3000',() => {
     console.log('App is running on PORT 3000');
