9465181688f5b5713510e74fae61dc04078534a7
HaiderMalik <haidermalik504@gmail.com>
2017-10-27 22:07:34 +0500

    created new company

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Company = require('./models/company.model');
 
+const bodyParser = require('body-parser');
 
 const MONGO_DB_URI = 'mongodb://localhost/y_mean';
-mongoose.connect(MONGO_DB_URI,{
-   useMongoClient: true
+mongoose.connect(MONGO_DB_URI, {
+    useMongoClient: true
 });
 
-mongoose.connection.on('connected',() => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connection.on('connected', () => {
     console.log('app is connected to mongodb ', MONGO_DB_URI);
 });
 
@@ -16,10 +21,30 @@ mongoose.connection.on('error', err => {
     console.log('error while connecting to mongoose ', err);
 });
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     return res.send('Welcome to MEAN App with Angular4+');
 });
 
-app.listen('3000',() => {
+app.post('/companies', (req, res) => {
+
+    if (!req.body.name) {
+        return res.status(400).send({err: 'name is required field'});
+    }
+
+    Company.create({
+        name: req.body.name,
+        city: req.body.city,
+        address: req.body.address
+    }, (err, savedCompany) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        return res.status(200).json(savedCompany);
+    })
+
+
+});
+
+app.listen('3000', () => {
     console.log('App is running on PORT 3000');
 });
